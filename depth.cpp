62b9600a4a3745da9f5a4b23294c9f9d72d9690f
ciiram <cwamaina.dekut@gmail.com>
2019-10-15 23:05:11 +0300

    Initial commit for water monitoring project

---
+++ depth.cpp
@@ -0,0 +1,85 @@
+#include "select_program.h"
+
+#if PROGRAM == DEPTH
+
+#include "mDot.h"
+#include "ChannelPlans.h"
+#include "MTSText.h"
+#include "device_addresses.h"
+#include "utils.h"
+
+#define     SLEEP_S          30 * 60
+
+mDot* dot = NULL;
+lora::ChannelPlan* plan = NULL;
+
+// sub band 1 - 8 will allow the radio to use the 8 channels in that sub band
+static uint8_t frequency_sub_band = 1;
+
+// deep sleep
+static bool deep_sleep = false;
+
+// retries 0 to disable acks
+static uint8_t ack = 0;
+
+// Parameters for ABP
+std::vector<uint8_t> network_address_vector(network_address, network_address + 4);
+std::vector<uint8_t> network_session_key_vector(network_session_key, network_session_key + 16);
+std::vector<uint8_t> data_session_key_vector(data_session_key, data_session_key + 16);
+
+
+
+static AnalogIn range_sensor(A0); // water level sensor
+const uint8_t DATA_RATE = 0;
+const uint32_t TX_POWER = 14;
+
+
+
+
+int main () {
+
+  plan = new lora::ChannelPlan_EU868();
+  assert(plan);
+
+  mDot* dot = mDot::getInstance(plan);
+  assert(dot);
+
+  setup_connection(ack, frequency_sub_band);
+
+  setup_abp_parameters(network_address_vector,
+                     network_session_key_vector,
+                     data_session_key_vector);
+
+
+  // Tx power
+  dot->setTxPower(TX_POWER);
+
+  // Data rate
+  dot->setTxDataRate(DATA_RATE);
+
+  while (1) {
+
+    uint16_t height;
+    height = range_sensor.read_u16();
+
+    printf("\rLevel: %d\n", height);
+
+    std::vector<uint8_t> tx_data;
+    tx_data.push_back((height >> 8) & 0xFF);
+    tx_data.push_back(height & 0xFF);
+
+    int32_t ret = dot->send(tx_data);
+    if (ret != mDot::MDOT_OK) {
+      printf("\r\nData transmission failed\n\r");
+      // return -1;
+    }
+
+    // sleep
+    dot->sleep(SLEEP_S, mDot::RTC_ALARM, deep_sleep);
+  }
+
+  return 0;
+}
+
+
+#endif
---
+++ select_program.h
@@ -0,0 +1,7 @@
+#ifndef _SELECT_PROGRAM_H
+#define _SELECT_PROGRAM_H
+
+#define DEPTH 0
+
+#define PROGRAM DEPTH
+#endif
