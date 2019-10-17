fbb6f9dab0b97f4d5656025d367629db728451f1
ciiram <cwamaina.dekut@gmail.com>
2019-10-17 13:33:50 +0300

    modified the setup code

--- depth.cpp
+++ depth.cpp
@@ -6,10 +6,10 @@
 #include "ChannelPlans.h"
 #include "MTSText.h"
 #include "device_addresses.h"
-#include "utils.h"
 
 
-#define     SLEEP_S          30 * 60
+
+#define     SLEEP_S          1 * 60
 
 mDot* dot = NULL;
 lora::ChannelPlan* plan = NULL;
@@ -45,11 +45,44 @@ int main () {
   mDot* dot = mDot::getInstance(plan);
   assert(dot);
 
-  setup_connection(ack, frequency_sub_band);
+  if (dot->getJoinMode() != mDot::MANUAL) {
+    printf("\r\nChanging Join Mode to Manual\n\r");
+    if (dot->setJoinMode(mDot::MANUAL) != mDot::MDOT_OK) {
+      printf("\r\nChanging join mode failed!\n\r");
+      return -1;
+    }
+  }
+  // Set frequency subband
+  if (dot->getFrequencySubBand() != frequency_sub_band) {
+    if (dot->setFrequencySubBand(frequency_sub_band) != mDot::MDOT_OK) {
+        printf("\r\nSetting frequency subband failed\n\r");
+        return -1;
+      }
+  }
 
-  setup_abp_parameters(network_address_vector,
-                     network_session_key_vector,
-                     data_session_key_vector);
+  // disable retries
+  if (dot->setAck(ack) != mDot::MDOT_OK) {
+    printf("\r\nSetting retries failed\n\r");
+    return -1;
+  }
+
+  // Set ABP parameters
+  if (dot->setNetworkAddress(network_address_vector) != mDot::MDOT_OK) {
+    printf("\r\nSetting device address failed\n\r");
+    return -1;
+  }
+
+  if (dot->setNetworkSessionKey(network_session_key_vector) != mDot::MDOT_OK) {
+    printf("\r\nSetting network session key failed\n\r");
+    return -1;
+  }
+
+  if (dot->setDataSessionKey(data_session_key_vector) != mDot::MDOT_OK) {
+    printf("\r\nSetting application session key failed\n\r");
+    return -1;
+  }
+
+
 
 
   // Tx power
--- depth_gps.cpp
+++ depth_gps.cpp
@@ -7,10 +7,10 @@
 #include "MTSText.h"
 #include "GPS.h"
 #include "device_addresses.h"
-#include "utils.h"
 
 
-#define     SLEEP_S          30 * 60
+
+#define     SLEEP_S          1 * 60
 
 mDot* dot = NULL;
 lora::ChannelPlan* plan = NULL;
@@ -47,12 +47,42 @@ int main () {
   mDot* dot = mDot::getInstance(plan);
   assert(dot);
 
-  setup_connection(ack, frequency_sub_band);
+  if (dot->getJoinMode() != mDot::MANUAL) {
+    printf("\r\nChanging Join Mode to Manual\n\r");
+    if (dot->setJoinMode(mDot::MANUAL) != mDot::MDOT_OK) {
+      printf("\r\nChanging join mode failed!\n\r");
+      return -1;
+    }
+  }
+  // Set frequency subband
+  if (dot->getFrequencySubBand() != frequency_sub_band) {
+    if (dot->setFrequencySubBand(frequency_sub_band) != mDot::MDOT_OK) {
+        printf("\r\nSetting frequency subband failed\n\r");
+        return -1;
+      }
+  }
 
-  setup_abp_parameters(network_address_vector,
-                     network_session_key_vector,
-                     data_session_key_vector);
+  // disable retries
+  if (dot->setAck(ack) != mDot::MDOT_OK) {
+    printf("\r\nSetting retries failed\n\r");
+    return -1;
+  }
 
+  // Set ABP parameters
+  if (dot->setNetworkAddress(network_address_vector) != mDot::MDOT_OK) {
+    printf("\r\nSetting device address failed\n\r");
+    return -1;
+  }
+
+  if (dot->setNetworkSessionKey(network_session_key_vector) != mDot::MDOT_OK) {
+    printf("\r\nSetting network session key failed\n\r");
+    return -1;
+  }
+
+  if (dot->setDataSessionKey(data_session_key_vector) != mDot::MDOT_OK) {
+    printf("\r\nSetting application session key failed\n\r");
+    return -1;
+  }
 
   // Tx power
   dot->setTxPower(TX_POWER);
@@ -68,10 +98,12 @@ int main () {
     printf("\rLevel: %d\n", height);
 
     //get gps
-    if (gps.sample() == 1) {
+    if (gps.sample()) {
       printf("\rLongitude: %.2f\t Latitude: %.2f\n",
              gps.longitude,
              gps.latitude);
+    } else {
+      printf("\r\n No GPS Lock\n\r");
     }
 
     std::vector<uint8_t> tx_data;
